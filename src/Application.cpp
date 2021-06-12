36698ca8084e45c418791637826dbeb3671a5298
Tom Anderson <tom@ando.gq>
2021-06-12 12:36:33 +1000

    GLEW and GLFW setup

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,46 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello" << std::endl;
-    std::cin.get();
+int main(void) {
+    GLFWwindow* window;
+
+    // Initialise glfw
+    if (!glfwInit()) return -1;
+
+    // Create the window
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Create the rendering context
+    glfwMakeContextCurrent(window);
+
+    // Initialise glew
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Problem initialising glew" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    // Run until the window is closed
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
